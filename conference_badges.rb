08b20347e4727fab04a5a7d0f5e918b2d2108d0f
Tyler Landry <tyler.landry1996@outlook.com>
2019-04-18 07:45:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,5 +13,5 @@ end
 def assign_rooms(speakers)
   badge_and_room = []
   speakers.each_with_index do |name, index|
-    badge_and_room << "Hello #{name}, you "
+    badge_and_room << "Hello #{name}, You'll be assigned to room #{index + 1}"
   
