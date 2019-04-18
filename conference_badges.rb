46123b0d545c23a1fb04e833af52648354c97da9
Tyler Landry <tyler.landry1996@outlook.com>
2019-04-18 07:42:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  badge_list = [] 
+  speakers.each do |name|
+  badge_list << "Hello, my name is #{name}."
+  end
+  badge_list
+end
+
+def assign_rooms
+  
